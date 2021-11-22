1e906e23e2cff87914c27e46364a355473d791c4
Yao Jean-Elisée <37454954+JeanElisee@users.noreply.github.com>
2021-11-22 23:40:25 +0100

    setting the folders to my gdrive

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # If it is to run on colab needs to change the location according to the dataset
-TRAIN_DIR = "data/train"
-VAL_DIR = "data/val"
-EVALUATION_DIR = "evaluation"
+TRAIN_DIR = "drive/MyDrive/pix2pix/dataset/maps/train"
+VAL_DIR = "drive/MyDrive/pix2pix/dataset/maps/val"
+EVALUATION_DIR = "pix_2_pix_using_c_gan/evaluation"
 LEARNING_RATE = 2e-4 # same as the paper
 BATCH_SIZE = 16 # Batch size of 16 in the paper 1
 NUM_WORKERS = 2
@@ -16,8 +16,8 @@ L1_LAMBDA = 100
 NUM_EPOCHS = 500
 LOAD_MODEL = False
 SAVE_MODEL = True
-CHECKPOINT_DISC = "disc.pth.tar"
-CHECKPOINT_GEN = "gen.pth.tar"
+CHECKPOINT_DISC = "drive/MyDrive/pix2pix/pretrained_weight/disc.pth.tar"
+CHECKPOINT_GEN = "drive/MyDrive/pix2pix/pretrained_weight/gen.pth.tar"
 
 both_transform = A.Compose(
     [A.Resize(width=256, height=256),], additional_targets={"image0": "image"},
